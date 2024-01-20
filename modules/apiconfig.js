0c82414387021c6b5ca1afa3030b32b9ef743cb5
Andreij1990 <151874330+Andreij1990@users.noreply.github.com>
2024-01-20 22:56:30 +0100

    Add files via upload
    apiconfig

---
+++ modules/apiconfig.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDiNJ1tt8kbcTC1nhdFUZ9K74ibNULPxOY",
+  authDomain: "versionshantering---gritsquare.firebaseapp.com",
+  projectId: "versionshantering---gritsquare",
+  storageBucket: "versionshantering---gritsquare.appspot.com",
+  messagingSenderId: "964614078781",
+  appId: "1:964614078781:web:525aa36210e026cc908554"
+};
