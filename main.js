9ee903c3c1d56b9f7ae0ff20cdbf3464e149439d
Andreij1990 <andre.i.jonsson@gritacademy.se>
2024-01-21 19:42:20 +0100

    ändringar

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 import {getMessages } from './modules/api.js';
 import {displayMessage, getRandomIntTop, getRandomIntLeft } from './modules/interface.js'; 
-import {messageInput, messagesContainer, sendMessage} from './modules/script.js';
+import {messageInput, messagesContainer, sendMessage} from './modules/messages.js';
 
 // getMessages();
 // console.log(getMessages());
 // console.log(getRandomIntTop());
 // console.log(getRandomIntLeft());
 
+document.getElementById("messageForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  sendMessage();
+});
 
 window.addEventListener("load", onPageLoad);
 
--- modules/messages.js
+++ modules/messages.js
@@ -3,11 +3,6 @@
 export const messageInput = document.getElementById("messageInput");
 export const messagesContainer = document.getElementById("messages");
 
-document.getElementById("messageForm").addEventListener("submit", (event) => {
-  event.preventDefault();
-  sendMessage();
-});
-
 export function sendMessage() {
     const messageDiv = document.createElement("div");
     messageDiv.innerText = messageInput.value;
