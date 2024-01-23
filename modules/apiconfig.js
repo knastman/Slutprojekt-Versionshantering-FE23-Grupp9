eb0a6ebddb46e06e00e1392e61d30a6cb0c3492f
Petra <144622676+knastman@users.noreply.github.com>
2024-01-23 23:57:56 +0100

    Testar separat restriktion

---
+++ modules/apiconfig.js
@@ -0,0 +1,22 @@
+
+// Firebase configuration OLD
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyDiNJ1tt8kbcTC1nhdFUZ9K74ibNULPxOY",
+//   authDomain: "versionshantering---gritsquare.firebaseapp.com",
+//   projectId: "versionshantering---gritsquare",
+//   storageBucket: "versionshantering---gritsquare.appspot.com",
+//   messagingSenderId: "964614078781",
+//   appId: "1:964614078781:web:525aa36210e026cc908554"
+// };
+
+
+// Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDiNJ1tt8kbcTC1nhdFUZ9K74ibNULPxOY",
+  authDomain: "versionshantering---gritsquare.firebaseapp.com",
+  databaseURL: "https://versionshantering---gritsquare-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "versionshantering---gritsquare",
+  storageBucket: "versionshantering---gritsquare.appspot.com",
+  messagingSenderId: "964614078781",
+  appId: "1:964614078781:web:525aa36210e026cc908554"
+};
