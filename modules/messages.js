e7943aee6f8877b5db6feeb77befa8104d4693e2
Alrik He <alrik_he@hotmail.com>
2024-01-25 10:38:31 +0100

    Update messages.js

--- modules/messages.js
+++ modules/messages.js
@@ -9,177 +9,175 @@ const errorContainer = document.querySelector("#errorContainer");
 *****************************************/
 
 export function displayMessage(messages) {
-    //Petra kolla koden här o rensa
-    const messageObj = messages.Messages;
-    // const messageObj2 = messages;
-    // console.log('messageObj');
-    // console.log(messageObj);
-    // console.log('messageObj2');
-    // console.log(messageObj2);
-    const messagesSection = document.querySelector(".messages");
-    // hideElements(allSections);
-    // movieListSection.classList.remove("hide");
+  //Petra kolla koden här o rensa
+  const messageObj = messages.Messages;
+  // const messageObj2 = messages;
+  // console.log('messageObj');
+  // console.log(messageObj);
+  // console.log('messageObj2');
+  // console.log(messageObj2);
+  const messagesSection = document.querySelector(".messages");
+  // hideElements(allSections);
+  // movieListSection.classList.remove("hide");
 
-    messagesSection.innerHTML = "";
-    let formattedTime = "";
+  messagesSection.innerHTML = "";
+  let formattedTime = "";
 
-    // Melker grupp 5 message of the day feature
-    const date = new Date();
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-    let currentDate = `${day}-${month}-${year}`;
+  // Melker grupp 5 message of the day feature
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let currentDate = `${day}-${month}-${year}`;
 
-    //Petras tillägg till melkers kod
-    let nrOfTrue = 0;
+  //Petras tillägg till melkers kod
+  let nrOfTrue = 0;
 
-    for (const messageid of Object.keys(messageObj).reverse()) {
-        const message = messageObj[messageid];
-        console.log(messageid);
-        // console.log('message');
-        // console.log(message);
+  for (const messageid of Object.keys(messageObj).reverse()) {
+    const message = messageObj[messageid];
+    // console.log('message');
+    // console.log(message);
 
-        const postTimestamp = message.timestamp;
-        const postDate = new Date(postTimestamp);
-        const hours = postDate.getHours().toString().padStart(2, "0");
-        const minutes = postDate.getMinutes().toString().padStart(2, "0");
+    const postTimestamp = message.timestamp;
+    const postDate = new Date(postTimestamp);
+    const hours = postDate.getHours().toString().padStart(2, "0");
+    const minutes = postDate.getMinutes().toString().padStart(2, "0");
 
-        formattedTime = `${hours}:${minutes} | ${postDate.getDate()}-${postDate.getMonth() + 1}-${postDate.getFullYear()}`;
+    formattedTime = `${hours}:${minutes} | ${postDate.getDate()}-${postDate.getMonth() + 1}-${postDate.getFullYear()}`;
 
-        const messageContainer = document.createElement("article");
-        messageContainer.classList.add("message");
+    const messageContainer = document.createElement("article");
+    messageContainer.classList.add("message");
 
-        const messageHeader = document.createElement("div");
-        const messageHeaderDiv1 = document.createElement("div");
-        const messageHeaderDiv2 = document.createElement("div");
-        const messageHeaderText = document.createElement("h3");
-        const messageBody = document.createElement("div");
-        //const messageText = document.createElement('p');
-        const messageText = document.createElement("input");
-        messageText.classList.add("EditableInput");
+    const messageHeader = document.createElement("div");
+    const messageHeaderDiv1 = document.createElement("div");
+    const messageHeaderDiv2 = document.createElement("div");
+    const messageHeaderText = document.createElement("h3");
+    const messageBody = document.createElement("div");
+    //const messageText = document.createElement('p');
+    const messageText = document.createElement("input");
+    messageText.classList.add("EditableInput");
 
-        const messageFooter = document.createElement("div");
-        const messageFooterDiv1 = document.createElement("div");
-        const messageFooterDiv2 = document.createElement("div");
+    const messageFooter = document.createElement("div");
+    const messageFooterDiv1 = document.createElement("div");
+    const messageFooterDiv2 = document.createElement("div");
 
-        messagesSection.append(messageContainer);
-        messageContainer.append(messageHeader, messageBody, messageFooter);
-        messageHeader.append(messageHeaderDiv1, messageHeaderDiv2);
-        messageHeaderDiv1.append(messageHeaderText);
-        messageBody.append(messageText);
-        messageFooter.append(messageFooterDiv1, messageFooterDiv2);
+    messagesSection.append(messageContainer);
+    messageContainer.append(messageHeader, messageBody, messageFooter);
+    messageHeader.append(messageHeaderDiv1, messageHeaderDiv2);
+    messageHeaderDiv1.append(messageHeaderText);
+    messageBody.append(messageText);
+    messageFooter.append(messageFooterDiv1, messageFooterDiv2);
 
-        messageHeader.classList.add("messageHeader");
-        messageBody.classList.add("messageBody");
-        messageFooter.classList.add("messageFooter");
+    messageHeader.classList.add("messageHeader");
+    messageBody.classList.add("messageBody");
+    messageFooter.classList.add("messageFooter");
 
-        messageHeaderText.innerText = message.name;
-        messageHeaderDiv2.innerText = formattedTime;
-        //messageText.innerText = message.text;
-        messageText.value = message.text;
-        messageText.addEventListener("keypress", async (event) => {
-            if (event.key === "Enter") {
-                alert("edited");
-                messageText.blur();
+    messageHeaderText.innerText = message.name;
+    messageHeaderDiv2.innerText = formattedTime;
+    //messageText.innerText = message.text;
+    messageText.value = message.text;
+    messageText.addEventListener("keypress", async (event) => {
+      if (event.key === "Enter") {
+        alert("edited");
+        messageText.blur();
 
-                console.log(messageid);
-                const postData = {
-                    text: messageText.value,
-                    timestamp: serverTimestamp(),
-                };
+        const postData = {
+          text: messageText.value,
+          timestamp: serverTimestamp(),
+        };
 
-                const messageSound = new Audio("./sound/message_sent.mp3");
-                messageSound.play();
+        const messageSound = new Audio("./sound/message_sent.mp3");
+        messageSound.play();
 
-                await set(ref(db, "posts/"+messageid), postData);
-            }
-        });
-        //Like message
-        const likeButton = document.createElement("button");
-        const showLikes = document.createElement("span");
-        likeButton.classList.add("likeButton");
-        messageFooterDiv1.appendChild(likeButton);
+        await set(ref(db, "posts/" + messageid), postData);
+      }
+    });
+    //Like message
+    const likeButton = document.createElement("button");
+    const showLikes = document.createElement("span");
+    likeButton.classList.add("likeButton");
+    messageFooterDiv1.appendChild(likeButton);
 
-        // likeButton.innerText = 'Gilla';
-        likeButton.innerHTML = '<i class="fa-regular fa-thumbs-up"></i>';
-        let likesTotal = message.likes || 0;
-        showLikes.innerText = likesTotal;
+    // likeButton.innerText = 'Gilla';
+    likeButton.innerHTML = '<i class="fa-regular fa-thumbs-up"></i>';
+    let likesTotal = message.likes || 0;
+    showLikes.innerText = likesTotal;
 
-        function clickLike() {
-            likesTotal++;
-            showLikes.innerText = likesTotal;
+    function clickLike() {
+      likesTotal++;
+      showLikes.innerText = likesTotal;
 
-            const updatedPostData = {
-                ...message,
-                likes: likesTotal,
-            };
+      const updatedPostData = {
+        ...message,
+        likes: likesTotal,
+      };
 
-            set(ref(db, `posts/${messageid}`), updatedPostData);
-        }
-        likeButton.addEventListener("click", clickLike);
-        likeButton.appendChild(showLikes);
-
-        //Delete messege
-        const deleteButton = document.createElement("button");
-        messageFooterDiv2.appendChild(deleteButton);
-        deleteButton.classList.add("deleteButton");
-        deleteButton.innerText = "Radera";
-
-        deleteButton.addEventListener("click", () => deleteMessage(messageid));
-
-        //Message of the day (contributor Melker)
-        const timeIncludesTime = formattedTime.includes(currentDate);
-        if (timeIncludesTime) {
-            nrOfTrue++; //petras add
-        }
+      set(ref(db, `posts/${messageid}`), updatedPostData);
     }
+    likeButton.addEventListener("click", clickLike);
+    likeButton.appendChild(showLikes);
 
-    // Melker grupp 5 message of the day feature
-    // const selected = messagesSection.querySelectorAll('article'); // Hämtar ALLA oavsett datum
-    const allArticles = messagesSection.querySelectorAll("article"); //Bytte namn för lättläslighet/logik
-    const randomIndex = Math.floor(Math.random() * nrOfTrue);
-    const randomSelected = allArticles[randomIndex];
-    // if(randomSelected){ // fyller ingen funktion då den alltid är sann /Petra
-    const messageOfTheDay = document.createElement("span"); //Ändrade från p
-    randomSelected.classList.add("messageOfTheDay");
-    randomSelected.append(messageOfTheDay);
-    // messageOfTheDay.innerText = 'Message of the day';
-    // messageOfTheDay.innerText = 'MOTD'; // hur hämtar jag footern i denna
-    // }
+    //Delete messege
+    const deleteButton = document.createElement("button");
+    messageFooterDiv2.appendChild(deleteButton);
+    deleteButton.classList.add("deleteButton");
+    deleteButton.innerText = "Radera";
+
+    deleteButton.addEventListener("click", () => deleteMessage(messageid));
+
+    //Message of the day (contributor Melker)
+    const timeIncludesTime = formattedTime.includes(currentDate);
+    if (timeIncludesTime) {
+      nrOfTrue++; //petras add
+    }
+  }
+
+  // Melker grupp 5 message of the day feature
+  // const selected = messagesSection.querySelectorAll('article'); // Hämtar ALLA oavsett datum
+  const allArticles = messagesSection.querySelectorAll("article"); //Bytte namn för lättläslighet/logik
+  const randomIndex = Math.floor(Math.random() * nrOfTrue);
+  const randomSelected = allArticles[randomIndex];
+  // if(randomSelected){ // fyller ingen funktion då den alltid är sann /Petra
+  const messageOfTheDay = document.createElement("span"); //Ändrade från p
+  randomSelected.classList.add("messageOfTheDay");
+  randomSelected.append(messageOfTheDay);
+  // messageOfTheDay.innerText = 'Message of the day';
+  // messageOfTheDay.innerText = 'MOTD'; // hur hämtar jag footern i denna
+  // }
 }
 
 /****************************************
 Formulär - meddelanden till databasen
 *****************************************/
 export function handlePostForm() {
-    const postForm = document.getElementById("messageForm");
-    const postTextInput = document.getElementById("postText");
+  const postForm = document.getElementById("messageForm");
+  const postTextInput = document.getElementById("postText");
 
-    postForm.addEventListener("submit", async (e) => {
-        e.preventDefault();
+  postForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-        const userName = document.getElementById("userName").value;
-        const postText = postTextInput.value;
+    const userName = document.getElementById("userName").value;
+    const postText = postTextInput.value;
 
-        const newPostRef = push(ref(db, "posts"));
-        const postData = {
-            name: userName,
-            text: postText,
-            timestamp: serverTimestamp(),
-            likes: 0,
-        };
+    const newPostRef = push(ref(db, "posts"));
+    const postData = {
+      name: userName,
+      text: postText,
+      timestamp: serverTimestamp(),
+      likes: 0,
+    };
 
-        /* Lunas kod */
-        const messageSound = new Audio("./sound/message_sent.mp3");
-        messageSound.play();
-        /* Lunas kod slut */
+    /* Lunas kod */
+    const messageSound = new Audio("./sound/message_sent.mp3");
+    messageSound.play();
+    /* Lunas kod slut */
 
-        await set(newPostRef, postData);
+    await set(newPostRef, postData);
 
-        //Rensar meddalndefältet
-        postTextInput.value = "";
-        postTextInput.focus();
-    });
+    //Rensar meddalndefältet
+    postTextInput.value = "";
+    postTextInput.focus();
+  });
 }
 
 /****************************************
@@ -187,16 +185,16 @@ export function handlePostForm() {
 *****************************************/
 
 export async function deleteMessage(messageid) {
-    if (confirm("Are you sure?")) {
-        try {
-            const messageRef = ref(db, `posts/${messageid}`);
+  if (confirm("Are you sure?")) {
+    try {
+      const messageRef = ref(db, `posts/${messageid}`);
 
-            await set(messageRef, null);
-        } catch (error) {
-            // } catch (displayError) {
-            console.error("Error deleting message:", error);
-        }
+      await set(messageRef, null);
+    } catch (error) {
+      // } catch (displayError) {
+      console.error("Error deleting message:", error);
     }
+  }
 }
 
 /********************************************
@@ -204,19 +202,19 @@ export async function deleteMessage(messageid) {
 *********************************************/
 
 export function displayError(error) {
-    let message;
-    const errorContainer = document.querySelector("#errorContainer");
-    hideElements(allSections);
-    errorContainer.classList.remove("hide");
+  let message;
+  const errorContainer = document.querySelector("#errorContainer");
+  hideElements(allSections);
+  errorContainer.classList.remove("hide");
 
-    if (error === 404) {
-        message = "Inga meddelanden hittades.";
-    } else {
-        message = "Något gick fel, försök igen.";
-    }
+  if (error === 404) {
+    message = "Inga meddelanden hittades.";
+  } else {
+    message = "Något gick fel, försök igen.";
+  }
 
-    const errorMessage = document.querySelector("#errorMessage");
-    errorMessage.innerText = message;
+  const errorMessage = document.querySelector("#errorMessage");
+  errorMessage.innerText = message;
 }
 
 //Nedan behövs troligen inte
@@ -226,5 +224,5 @@ export function displayError(error) {
 **********************************/
 
 function hideElements(array) {
-    array.forEach((element) => element.classList.add("hide"));
+  array.forEach((element) => element.classList.add("hide"));
 }
