2d0fa53f52b499246f86dfa67e8bc4c30bfa20bb
Gnidleif <gnidleif@gmail.com>
2024-01-26 22:42:15 +0100

    Removed some test code

--- modules/profanity.js
+++ modules/profanity.js
@@ -11,22 +11,15 @@ const filtered_words = [
 const pattern = new RegExp(`(${filtered_words.join('|')})`, "ig");
 
 export function profanityFilter(sentence) {
-    let foundmatch = false;
     let arr = [];
     let filtered = JSON.parse(JSON.stringify(sentence));
     while ((arr = pattern.exec(sentence)) !== null) {
-        foundmatch = true;
         const match = arr[0];
         const idx = arr.index;
         const first = filtered.slice(0, idx + 1);
         const stars = "*".repeat(match.length - 2);
         const last = filtered.slice(idx + (match.length - 1));
         filtered = `${first}${stars}${last}`;
-    }
-
-    if (foundmatch) {
-        console.log(sentence);
-        console.log(sentence);
     }
 
     return filtered;
